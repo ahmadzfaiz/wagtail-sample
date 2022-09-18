c4172e7b57eb787b0b3bc702bd139a2e997fa478
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-09-18 17:48:49 +0700

    Initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rvk)hj2c4l^@+n3)1$bh6)zj5aq211+(doc6@i=iu9br0@!#)9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
